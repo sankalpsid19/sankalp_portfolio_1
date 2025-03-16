1534b62ac9d9105a80be254bbcb2373c48d53d8e
sankalpsid19 <sankalpyadav96@gmail.com>
2025-03-16 10:26:45 +0530

    import bug fix

--- src/components/Resume/ResumeNew.tsx
+++ src/components/Resume/ResumeNew.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "../../Assets/../Assets/Sankalp_Yadav.pdf";
+import pdf from "../../assets/Sankalp_Yadav.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
