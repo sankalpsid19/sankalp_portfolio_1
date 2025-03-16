64cd7526b37e11d473448fec78369decfa47d468
sankalpsid19 <sankalpyadav96@gmail.com>
2025-03-16 10:23:42 +0530

    added resume

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
 import About from "./components/About/About";
+import Resume from "./components/Resume/ResumeNew";
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -28,6 +29,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+
           {/* <Route path="/resume" element={<Resume />} />
           <Route path="*" element={<Navigate to="/" />} /> */}
         </Routes>
